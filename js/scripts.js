bf6c44820382e47526751d43a93a255b274e8a16
Mohammad <mhmmdrzaei@gmail.com>
2022-01-28 18:56:04 -0500

    ajax call mostly

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,83 @@
-$(function(){
+const gearApp= {
 
-	// console.log("It's working");
+}
+
+// gearApp.getPieces =function(query) {
+
 	$.ajax({
 	url: 'https://tsv.amsnetwork.ca/api/v3/assets?type=Equipment&per_page=600',
 	type: 'GET',
 	beforeSend: function (xhr) {
 	    xhr.setRequestHeader('Authorization', 'Bearer e250cc61ae026bc9ad3c4c616c06b4cf99018fee7a6da149fc3528e591bf77d4');
 	},
-	data: {},
-	success: function () { },
+	data: {
+	},
+	success: function (res) { 
+		console.log(res.assets);
+		$.each(res, function(index, value) {  
+			for(var i=0; i<1000; i++){
+				var available =value[i].status_text;
+				var name = value[i].name;
+				var catagory = value[i].category_name;
+				if (catagory === "Lights") {
+					console.log (name);
+					 $('.output').append('<div>'+name+'</div>');
+				}
+				// 
+				// console.log (name);
+				// $('.output').append(name);
+
+			}
+			
+		});
+
+	},
 	error: function () { },
 	}).then(function(res) {
-  console.log(res);
 
 
+
+  // console.log(res.assets);
+  // $('gearList').empty();
+  // gearApp.displayPieces(res.assets);
+
+  	// console.log(i);
+
+  	
+    
+     
+   
+    
+    
+    // var bio = value.user.bio;
+    // var imageURL = value.urls.regular;
+    
+    // $('.name').text(name);
+    // $('.bio').text(bio);
+    // $('.image img').attr('src', imageURL);
+    
+ 
 });
+
+
+
+// };
+
+// gearApp.displayPieces = function(data){
+// 	const gearHTML = data.filter((gearObj) => gearObj.photo).map((gearObj) => {
+// 		let gearPieces = `${gearObj.title}`;
+// 		return gearPieces;
+// 	}).join('');
+// 	$('#gearList').append(gearPieces);
+// 	console.log(gearPieces)
+// }
+
+
+
+$(function(){
+
+	// console.log("It's working");
+
 //   if($('ul#archiveEach').children.length === 1) { 
 //     $("ul#archiveEach li").css("background", "#F00");
 //   }
@@ -27,6 +90,10 @@ $('ul#archiveEach').each(function() {
   }
 });
 });
+
+
+
+
 // var each = document.getElementById('archiveEach').getElementsByTagName('li');
 
 
--- page-rentals.php
+++ page-rentals.php
@@ -1,22 +1,40 @@
 <?php get_header();  ?>
+<main class="rentalsPage" id="rentalsPage">
+  
+  <?php // Start the loop ?>
+  <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+  <h1 class="pageTitle"><?php the_title(); ?></h1>
+  <section class="pageSingleContentBox">
+  <?php if ( has_post_thumbnail() ) { ?>
+   <figure class="pageSignleImage">
+     <?php the_post_thumbnail('large');?>
+   </figure>
+   <section class="pageSingleContentHalf">
+  <?php }else {; ?>
+    <section class="pageSingleContent">
+  <?php  };?>
+    <?php the_content(); ?>
+    </section>
+  <form action="">
+    <label for="animal">Choose your animal:</label>
+    <select name="animal" id="animal">
+      <option value="monkey">Monkeys</option>
+      <option value="eagle">Eagles</option>
+      <option value="dragon">Dragons</option>
+      <option value="elephant">Elephants</option>
+      <option value="centaur">Centaur</option>
+    </select>
+  </form>
+  <main>
+    <div class="output">
+      
+    </div>
+    <div class="container" id="gearList"></div>
+  </main>
+  </section> 
 
-<div class="main">
-  <div class="container">
-
-    <div class="content">
-      <?php // Start the loop ?>
-
-      <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-
-        <h2><?php the_title(); ?></h2>
-        <?php the_content(); ?>
-
-      <?php endwhile; // end the loop?>
-    </div> <!-- /,content -->
-
-    <?php get_sidebar(); ?>
-
-  </div> <!-- /.container -->
-</div> <!-- /.main -->
+  <?php endwhile; // end the loop?>
+  
+</main>
 
 <?php get_footer(); ?>
