e7083ab5c5fc290ffcb6e7674365d9ec486714e1
Mohammad <mhmmdrzaei@gmail.com>
2022-12-02 19:25:04 -0500

    ajax call

--- js/scripts-old.js
+++ js/scripts-old.js
@@ -3,154 +3,173 @@
 
 
 $(function(){
-	if($('body').hasClass('page-id-16712')) {
-			$.ajax({
-			url: 'https://tsv.amsnetwork.ca/api/v3/assets?type=Equipment&per_page=600',
-			type: 'GET',
-			beforeSend: function (xhr) {
-			    xhr.setRequestHeader('Authorization', 'Bearer 76804e015e15894372faf9623c8c036dd91661ab41fae379c4578abe28723dd7');
-			},
-			data: {
-			},
-			success: function (res) { 
-				// console.log(res.assets);
+
+		$.ajax({
+		url: 'https://tsv.amsnetwork.ca/api/v3/assets?type=Equipment&per_page=600',
+		type: 'GET',
+		beforeSend: function (xhr) {
+		    xhr.setRequestHeader('Authorization', 'Bearer 76804e015e15894372faf9623c8c036dd91661ab41fae379c4578abe28723dd7');
+		},
+		data: {
+		},
+		success: function (res) { 
+			// console.log(res.assets);
 				$('.output').empty();
-				let gear = 'Cameras';
-				$.each(res, function(index, value) { 
-					$("#gearOptions").on("change", function(){
-						$('.output').empty();
-						const choice = $('select option:selected').text();
-						// $('#page-title').text(choice);
-					  var gearUpdate = $(this).val();
-					  var gear = gearUpdate;			
-					  for(var i=0; i<1000; i++){
-						var available =value[i].status_text;
-						var name = value[i].name;
-						var catagory = value[i].category_name;
-						var image = value[i].photo_medium;
-						var description = value[i].description || '';
-						var external_url_resources = value[i].external_url_resources;
-						var included_accessories = value[i].included_accessories || '';
-						let price =  value[i].price_types;
+
+			// window.myvar = res.assets;
+			// var gear;
+			let gear = 'Cameras';
+			$.each(res, function(index, value) { 
+				$("#gearOptions").on("change", function(){
+					console.log('test');
+					$('.output').empty();
+					const choice = $('select option:selected').text();
+					// $('#page-title').text(choice);
+				  var gearUpdate = $(this).val();
+				  var gear = gearUpdate;
+				  console.log(gear);			
+				  for(var i=0; i<1000; i++){
+					var available =value[i].status_text;
+					var name = value[i].name;
+					var catagory = value[i].category_name;
+					var image = value[i].photo_medium;
+					var description = value[i].description || '';
+					var external_url_resources = value[i].external_url_resources;
+					var included_accessories = value[i].included_accessories || '';
+					let price =  value[i].price_types;
+
+					// var priceDay = parseInt(price.replace(/[^0-9\.]/g, ''), 10);
 
 
-						if ((catagory === gear) && (available === 'Active') ) {
-						var priceDay = parseInt(price.toString().replace(/[^0-9\.]/g, ''));
-						var PriceDayOne = priceDay * 1;
-						var priceWeek = priceDay * 4;
-						var priceMonth = priceDay * 16;
-						var artPieceHtml = `
-						  <article class="gearEach">
-						  <section class="gearEachInner">
-						    <h2>${name}</h2>
-						    <figure><img src="${image}"></figure>
-						    <section class="pricesection">
-						      <section class="priceDay">$${priceDay}/DAY</section>
-						      <section class="priceweek">$${priceWeek}/WEEK</section>
-						      <section class="priceMonth">$${priceMonth}/MONTH</section>
-						    </section>
-						    <button class="specsAvail">More Information</button>
-						  </section>  
-						  	 <section class="gearEachMore">
-						  	 <section class="gearMoreInner">
-						  	 	<section class="gearTitle">
-						  	 		<h2>${name}</h2>
-						  	 		<span>X</span>
-						  	 	</section>
-						  	 	<figure><img src="${image}"></figure>
-						  	 	<section class="gearinfoDesc">
-						  	 	${description ?`<section class="descriptionGear">
-						  	 	<h4>Description:</h4>${description}</section>` : ''} 
-						  	 	${included_accessories ?`<section class="accInc">
-						  	 	<h4>Accessories Included:</h4>${included_accessories}</section>` : ''} 
-						  	 	${external_url_resources ? `<a href="${external_url_resources}" target="_blank">Additional Gear Information</a>` : ''}
-						  	 	
-						  	 	<button class="openGCF">Open Gear Rental Request Form</button>
-						  	 	</section>
+					if ((catagory === gear) && (available === 'Active') ) {
+					var priceDay = parseInt(price.toString().replace(/[^0-9\.]/g, ''));
+					var PriceDayOne = priceDay * 1;
+					var priceWeek = priceDay * 4;
+					var priceMonth = priceDay * 16;
+					var artPieceHtml = `
+					  <article class="gearEach">
+					  <section class="gearEachInner">
+					    <h2>${name}</h2>
+					    <figure><img src="${image}"></figure>
+					    <section class="pricesection">
+					      <section class="priceDay">$${priceDay}/DAY</section>
+					      <section class="priceweek">$${priceWeek}/WEEK</section>
+					      <section class="priceMonth">$${priceMonth}/MONTH</section>
+					    </section>
+					    <button class="specsAvail">More Information</button>
+					  </section>  
+					  	 <section class="gearEachMore">
+					  	 <section class="gearMoreInner">
+					  	 	<section class="gearTitle">
+					  	 		<h2>${name}</h2>
+					  	 		<span>X</span>
+					  	 	</section>
+					  	 	<figure><img src="${image}"></figure>
+					  	 	<section class="gearinfoDesc">
+					  	 	${description ?`<section class="descriptionGear">
+					  	 	<h4>Description:</h4>${description}</section>` : ''} 
+					  	 	${included_accessories ?`<section class="accInc">
+					  	 	<h4>Accessories Included:</h4>${included_accessories}</section>` : ''} 
+					  	 	${external_url_resources ? `<a href="${external_url_resources}" target="_blank">Additional Gear Information</a>` : ''}
+					  	 	
+					  	 	<button class="openGCF">Open Gear Rental Request Form</button>
+					  	 	</section>
 
 
-						  	 </section>
-						   
-						   </section>
-						      
-						  </article>  
+					  	 </section>
+					   
+					   </section>
+					      
+					  </article>  
 
-						    `;
+					    `;
 
-							 $('.output').append(artPieceHtml);
-						}
-
-					} });
-					for(var i=0; i<1000; i++){
-						var available =value[i].status_text;
-						var name = value[i].name;
-						var catagory = value[i].category_name;
-						var image = value[i].photo_medium;
-						var description = value[i].description || '';
-						var external_url_resources = value[i].external_url_resources || '';
-						var external_two = `<a href="${external_url_resources}" target="_blank">Additional Gear Information</a>` || '';
-						var included_accessories = value[i].included_accessories || '';
-						// var fullAccessories = `Hello ${included_accessories ? included_accessories : 'unknown'}`;
-						let price =  value[i].price_types;
-
-						if ((catagory === gear) && (available === 'Active') ) {
-						var priceDay = parseInt(price.toString().replace(/[^0-9\.]/g, ''));
-						var PriceDayOne = priceDay * 1;
-						var priceWeek = priceDay * 4;
-						var priceMonth = priceDay * 16;
-						var artPieceHtml = `
-						  <article class="gearEach">
-						  <section class="gearEachInner">
-						    <h2>${name}</h2>
-						    <figure><img src="${image}"></figure>
-						    <section class="pricesection">
-						      <section class="priceDay">$${priceDay}/DAY</section>
-						      <section class="priceweek">$${priceWeek}/WEEK</section>
-						      <section class="priceMonth">$${priceMonth}/MONTH</section>
-						    </section>
-						    <button class="specsAvail">More Information</button>
-						  </section>  
-						  	 <section class="gearEachMore">
-						  	 <section class="gearMoreInner">
-						  	 	<section class="gearTitle">
-						  	 		<h2>${name}</h2>
-						  	 		<span>X</span>
-						  	 	</section>
-						  	 	<figure><img src="${image}"></figure>
-						  	 	<section class="gearinfoDesc">
-						  	 	${description ?`<section class="descriptionGear">
-						  	 	<h4>Description:</h4>${description}</section>` : ''} 
-						  	 	${included_accessories ?`<section class="accInc">
-						  	 	<h4>Accessories Included:</h4>${included_accessories}</section>` : ''} 
-						  	 	${external_url_resources ? `<a href="${external_url_resources}" target="_blank">Additional Gear Information</a>` : ''}
-						  	 	
-						  	 	<button class="openGCF">Open Gear Rental Request Form</button>
-						  	 	</section>
-
-
-						  	 </section>
-						   
-						   </section>
-						      
-						  </article>  
-
-						    `;
-
-							 $('.output').append(artPieceHtml);
-						}
-
+						 $('.output').append(artPieceHtml);
 					}
-					
+					// 
+					// console.log (name);
+					// $('.output').append(name);
+
+				}
+
+				  
+				  // gearApp.getPieces(gear);
+				  // gearApp.updateTitle();
 				});
+				for(var i=0; i<1000; i++){
+					var available =value[i].status_text;
+					var name = value[i].name;
+					var catagory = value[i].category_name;
+					var image = value[i].photo_medium;
+					var description = value[i].description || '';
+					var external_url_resources = value[i].external_url_resources || '';
+					var external_two = `<a href="${external_url_resources}" target="_blank">Additional Gear Information</a>` || '';
+					var included_accessories = value[i].included_accessories || '';
+					// var fullAccessories = `Hello ${included_accessories ? included_accessories : 'unknown'}`;
+					let price =  value[i].price_types;
+
+					if ((catagory === gear) && (available === 'Active') ) {
+					var priceDay = parseInt(price.toString().replace(/[^0-9\.]/g, ''));
+					var PriceDayOne = priceDay * 1;
+					var priceWeek = priceDay * 4;
+					var priceMonth = priceDay * 16;
+					var artPieceHtml = `
+					  <article class="gearEach">
+					  <section class="gearEachInner">
+					    <h2>${name}</h2>
+					    <figure><img src="${image}"></figure>
+					    <section class="pricesection">
+					      <section class="priceDay">$${priceDay}/DAY</section>
+					      <section class="priceweek">$${priceWeek}/WEEK</section>
+					      <section class="priceMonth">$${priceMonth}/MONTH</section>
+					    </section>
+					    <button class="specsAvail">More Information</button>
+					  </section>  
+					  	 <section class="gearEachMore">
+					  	 <section class="gearMoreInner">
+					  	 	<section class="gearTitle">
+					  	 		<h2>${name}</h2>
+					  	 		<span>X</span>
+					  	 	</section>
+					  	 	<figure><img src="${image}"></figure>
+					  	 	<section class="gearinfoDesc">
+					  	 	${description ?`<section class="descriptionGear">
+					  	 	<h4>Description:</h4>${description}</section>` : ''} 
+					  	 	${included_accessories ?`<section class="accInc">
+					  	 	<h4>Accessories Included:</h4>${included_accessories}</section>` : ''} 
+					  	 	${external_url_resources ? `<a href="${external_url_resources}" target="_blank">Additional Gear Information</a>` : ''}
+					  	 	
+					  	 	<button class="openGCF">Open Gear Rental Request Form</button>
+					  	 	</section>
 
 
-			},
-			error: function () { },
+					  	 </section>
+					   
+					   </section>
+					      
+					  </article>  
+
+					    `;
+
+						 $('.output').append(artPieceHtml);
+					}
+
+					// 
+					// console.log (name);
+					// $('.output').append(name);
+
+				}
+				
 			});
 
-	}
 
-		
+		},
+		error: function () { },
+		}).then(function() {
+
+	    
+	 
+	});
 
 	$(document).on('click', '.gearEach', function(){ 
     $(this).find('.gearEachMore').toggleClass('gearOpen');
@@ -198,7 +217,13 @@ $('ul#archiveEach').each(function() {
   }
 });
 
+				// $("button.specsAvail").each(function(){
+				//   $(this).click(function() {
+				//   $('.gearEach').find(".gearEachMore").toggle(1000);
+				//   console.log('test');
+				//  }); 
 
+				// });
 $('.gearEach').on('click', 'button.specsAvail', function() {
 	console.log('fuck');
 });
